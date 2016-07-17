b56c7a63076331a27582bf24a03fd48590257a7a
handriss <andrashinkel@gmail.com>
2016-07-18 01:27:08 +0200

    Write method added for modifying the config.json file

--- config.py
+++ config.py
@@ -12,3 +12,18 @@ class Config():
                 return config[key]
         except KeyError or ValueError as e:
             print("No such config option!", e)
+
+    @classmethod
+    def write(cls, key, value):
+        try:
+            with open(cls.config_file, "r") as f:
+                data = json.load(f)
+                f.close()
+        except KeyError or ValueError as e:
+            return ""
+
+        data[key] = value
+
+        with open(cls.config_file, "w+") as f:
+            f.write(json.dumps(data))
+            f.close
